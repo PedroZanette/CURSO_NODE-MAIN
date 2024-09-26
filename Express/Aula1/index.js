cc55f6945db7a3315d11a02c1347c34b4aafd84d
PEDRO HENRIQUE NUNES ZANETTE <pedro_zanette@estudante.sesisenai.org.br>
2024-09-26 09:54:07 -0300

    inital commit

--- Express/Aula1/index.js
+++ Express/Aula1/index.js
@@ -1,2 +1,25 @@
 const express = require('express');
+const app = express();
+const port = 3000 //variável ambiente
 
+const path = require("path");
+const basepath = path.join(__dirname, 'Aula1')
+const checkAuth = function(req, res, next){
+    req.authStatus = true;
+    if(req.authStatus){
+        console.log("Usuário logado");
+    } else {
+        console.log("SUS!")
+    }
+};
+
+app.use(checkAuth);
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basepath}/index.html`);
+
+})
+
+app.listen(port, () => {
+    console.log(`App não explodiu "ainda"!\nPorta: ${port}`) 
+})
